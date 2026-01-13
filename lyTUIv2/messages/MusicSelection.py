33da4656394b08350d9df248a65355136ae994ed
hydra <16paysayocesar@gmail.com>
2026-01-13 11:50:24 +0100

    ajout de message pour selection music

--- lyTUIv2/messages/MusicSelection.py
+++ lyTUIv2/messages/MusicSelection.py
@@ -1,6 +1,7 @@
 from textual.message import Message
+from models.tracks import Track
 
 class MusicSelection(Message) :
-    def __init__(self, music : dict):
+    def __init__(self, music : Track):
         self.music = music
         super().__init__()
