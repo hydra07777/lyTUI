9333494b51913a0572e5bd74fdcd8d87e8924612
hydra <16paysayocesar@gmail.com>
2026-01-13 11:51:24 +0100

    refacto de Music panel et ResultsPanel

--- lyTUIv2/widgets/MusicPanel.py
+++ lyTUIv2/widgets/MusicPanel.py
@@ -1,10 +1,13 @@
 from textual.containers import Vertical
 from textual.widgets import Static, ProgressBar
+from widgets.lyrics_view import LyricsView
 
 class MusicPanel(Vertical):
     def compose(self):
         yield Static("no Music selected")
         yield ProgressBar(total= 300, id="progress")
-        yield L
+        yield Static("touche")
+        yield LyricsView()
+
     def display_music_cover(self, music):
         pass
--- lyTUIv2/widgets/ResultsPanel.py
+++ lyTUIv2/widgets/ResultsPanel.py
@@ -1,20 +1,29 @@
 from textual.containers import Vertical
 from textual.widgets import ListView, ListItem, Label, Static
+from models.tracks import Track
+from textual import on
+from messages.MusicSelection import MusicSelection
 
 class ResultsPanel(Vertical):
 
     def compose(self):
         yield Static('ici et m')
 
-    def show_results(self, id: str, results):
+    def show_results(self, id: str, results : list[Track]):
         self.remove_children()
         label = Label(f"Résultats : {id}")
         lv = ListView(
-            *[ListItem(Label(str(r["title"]))) for r in results]
+            *[ListItem(Label(str(track.title)), id=f"{results.index(track)}") for track in results]
         )
         self.mount(label)
         self.mount(lv)
 
-        print(lv)
+    @on(ListView.Selected)
+    def on_track_selected( self, event : ListView.Selected):
+        value = event.item.id
+
+        self.post_message(MusicSelection(value))
+        
+    
 
 # ResultsPanel().show_results('all', [{'title': 'Un homme', 'artist': 'Jérémy Frerot', 'album': 'Un homme', 'year': 2025}])
