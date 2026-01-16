e332b6130d0eeed6322194bd7ddad7e372b6d064
hydra <16paysayocesar@gmail.com>
2026-01-16 17:32:27 +0100

    ajout de l'algorithm de changement de la cover en ANSI

--- lyTUIv2/services/ansi_converter.py
+++ lyTUIv2/services/ansi_converter.py
@@ -11,28 +11,29 @@ def resize_image(image, width):
     height = int(width * ratio * 0.5)
     return image.resize((width, height), Image.Resampling.LANCZOS)
 
-def convert_cover_ansi(cover):
-    if not cover :
+def convert_cover_ansi(cover_byte):
+    if not cover_byte:
         return "Pas de cover"
+
     try:
-        image = Image.open(io.BytesIO(cover)).convert("RGB")
-        image = resize_image(cover, 50)
+        image = Image.open(io.BytesIO(cover_byte)).convert("RGB")
+        image = resize_image(image, 20)
+
         pixels = image.load()
         lines = []
 
-        for y in range( image.height) :
+        for y in range(image.height):
             line = ""
             for x in range(image.width):
                 r, g, b = pixels[x, y]
                 luminance = int(0.2126*r + 0.7152*g + 0.0722*b)
-                char = UNICODE_GRADIENT [
-                    luminance * (len(UNICODE_GRADIENT) -1 ) // 225
+                char = UNICODE_GRADIENT[
+                    luminance * (len(UNICODE_GRADIENT) - 1) // 255
                 ]
                 line += f"\x1b[38;2;{r};{g};{b}m{char}\x1b[0m"
-        
             lines.append(line)
+
         return "\n".join(lines)
 
     except Exception as e:
-         return f"Error generating ASCII art: {e}"
-    
+        return f"Error generating ASCII art: {e}"
