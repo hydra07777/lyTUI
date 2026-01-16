2a786eba709a5bda3c88b9703b2a1d56f637c00e
hydra <16paysayocesar@gmail.com>
2026-01-16 17:30:35 +0100

    refacto de l'affichage de la cover

--- lyTUIv2/widgets/MusicPanel.py
+++ lyTUIv2/widgets/MusicPanel.py
@@ -1,13 +1,21 @@
 from textual.containers import Vertical
 from textual.widgets import Static, ProgressBar
 from widgets.lyrics_view import LyricsView
+from models.tracks import Track
+from services.ansi_converter import convert_cover_ansi
+from rich.text import Text
 
 class MusicPanel(Vertical):
     def compose(self):
-        yield Static("no Music selected")
+        yield Static("no Music selected", id="test")
         yield ProgressBar(total= 300, id="progress")
         yield Static("touche")
         yield LyricsView()
 
     def display_music_cover(self, music):
         pass
+
+    def show_music_selected(self, track : Track) :
+        cover = convert_cover_ansi(track.cover_byte)
+        text = Text.from_ansi(cover)
+        self.query_one("#test").update(text)
