d30417b2b0ac06bd2c0c0d8c1d6589c17eee6bd7
hydra <16paysayocesar@gmail.com>
2026-01-17 18:10:03 +0100

    refacto de MusicLoader avec support natif des majeurs parties des extensions

--- lyTUIv2/audio/MusicLoader.py
+++ lyTUIv2/audio/MusicLoader.py
@@ -1,6 +1,7 @@
 import os
 from mutagen.id3 import ID3
 from models.tracks import Track
+from services.get_metadata import get_metadata
 
 class MusicLoader():
 
@@ -9,23 +10,23 @@ class MusicLoader():
         self.music : Track = None
     
     def load_music_from_dir(self, folder : str):
+        SUPPORTED = (".mp3", ".m4a", ".wav", ".flac", ".ogg")
         id = 0
-        
         for file in sorted(os.listdir(folder)):
         
-            if file.lower().endswith(".mp3") :
-                music = ID3(os.path.join(folder, file))
-                cover = music.get('APIC:')
-                audio =  music.get('TIT2', 'Unknown').text[0]
+            if file.lower().endswith(SUPPORTED) :
+                path = os.path.join(folder, file)
+                meta = get_metadata(path=path)
                 self.music = Track(id,
-                                   os.path.join(folder, file),
-                                   music.get('TIT2', 'Unknown').text[0],
-                                   music.get('TALB', 'Unknown').text[0],
-                                   music.get('TPE2', 'Unknown').text[0],
-                                   cover.data if cover else None
+                                   path,
+                                   meta["title"],
+                                   meta["album"],
+                                   meta["artist"],
+                                   meta["cover"]
                 )
 
                 self.musics.append(self.music)
+                id += 1
         print(self.musics)
 
 
