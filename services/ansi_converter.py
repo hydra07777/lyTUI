e9e19562543e4190708fd8c9ae24cdd154d791e4
hydra <16paysayocesar@gmail.com>
2026-01-19 21:39:29 +0100

    hot-font ratio:55

--- services/ansi_converter.py
+++ services/ansi_converter.py
@@ -17,7 +17,7 @@ def convert_cover_ansi(cover_byte):
 
     try:
         image = Image.open(io.BytesIO(cover_byte)).convert("RGB")
-        image = resize_image(image, 50)
+        image = resize_image(image, 55)
 
         pixels = image.load()
         lines = []
