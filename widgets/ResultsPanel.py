c31d35acee86a025f11a65180892c1a741efb69a
hydra <16paysayocesar@gmail.com>
2026-07-29 14:31:10 +0100

    feat(ResultsPanel) : add bidding for resultpanel component

--- widgets/ResultsPanel.py
+++ widgets/ResultsPanel.py
@@ -3,8 +3,13 @@ from textual.widgets import ListView, ListItem, Label, Static
 from models.tracks import Track
 from textual import on
 from messages.MusicSelection import MusicSelection
+from messages.TrackActionsRequested import TrackActionsRequested
 
 class ResultsPanel(Vertical):
+
+    BINDINGS = [
+        ("o", "open_track_actions", "Actions"),
+    ]
 
     def compose(self):
         yield Static('ici et m')
@@ -23,6 +28,19 @@ class ResultsPanel(Vertical):
         id = int(event.item.name)
 
         self.post_message(MusicSelection(id))
+
+    def action_open_track_actions(self):
+        try:
+            lv = self.query_one(ListView)
+        except Exception:
+            return
+
+        item = lv.highlighted_child
+        if item is None or item.name is None:
+            return
+
+        track_id = int(item.name)
+        self.post_message(TrackActionsRequested(track_id))
         
     
 
